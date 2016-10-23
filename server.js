a19370d0e4378786e31aa9047430abdd59bec287
Nicolás Battaglia <ing.nicolas.battaglia@gmail.com>
2016-10-23 11:10:19 -0300

    config

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ var express = require('express');
 
 // Start Server
 var app = express();
-var port = process.env.PORT || 3000 ;
+//
+
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+var appEnv = cfenv.getAppEnv();
+var port = appEnv.PORT || 3000 ;
+
 // Public path configuration
 app.use(express.static(__dirname + '/app'));
 // parse application/x-www-form-urlencoded
 
 // Start App
-console.log('Starting App... \nlistening on port ' + port);
-app.listen(port);
+
+app.listen(port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
